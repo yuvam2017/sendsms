a7b1f4ae210e07b801438928080876ac3ef3cf98
Yuvam <67573209+yuvampython@users.noreply.github.com>
2020-12-09 20:56:36 +0530

    Update send_sms_using_python.py

--- send_sms_using_python.py
+++ send_sms_using_python.py
@@ -2,12 +2,12 @@
 import requests
 url = "https://www.fast2sms.com/dev/bulk"
 message = " Welcome to delhi"
-number = "+919450524879"
+number = "+911234567890" # Enter your Registered mobile number on the FAST2SMS It gives you 50rupees as credit on registering
 payload = f"sender_id=FastWP&message={message}&language=english&route=p&numbers={number[3:]}"
 headers = {
-'authorization': "BqUAVMIgdW4oZs7zkNfPbwcv3HQ6EexCS9FmX1DulTyLiJGr5KotVqHTSc01Onh4kGaURwfuB5Qg893l",
+'authorization': "YOUR API HERE",
 'Content-Type': "application/x-www-form-urlencoded",
 'Cache-Control': "no-cache",
 }
 response = requests.request("POST", url, data=payload, headers=headers)
-print(response.text)
+print(response.text) # TO check whether your code run correctly
